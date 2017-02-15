0ab7af1c25e763ea9f8c7c16bf571869bfd28fff
Victor Gama <hey@vito.io>
2017-02-15 15:44:41 -0200

    Update frontend facilities

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -22,9 +22,10 @@
 
   let nextProcess = FULL;
   let halfNumber = 0;
+  let currentIndex = 0;
   function process(arr) {
     return arr.map(r => {
-      debugger;
+      currentIndex++;
       if(r.data.type === 'rich-link') {
         const reactions = r.reactions.map(processReaction).join('');
         const data = r.data.data;
@@ -41,6 +42,24 @@
           perRow: halfsQtyForScreen,
           url: data.url,
         }
+      } else if(r.data.type === 'poor-link') {
+        const data = r.data.data;
+        if(!r.data || `${r.data}`.trim().length < 1) {
+          return null;
+        }
+        const reactions = r.reactions.map(processReaction).join('');
+        return {
+          backgroundColor: ['#0E0B06', '#FBF5EE'][currentIndex % 2],
+          primaryColor:    ['#6D87AE', '#525252'][currentIndex % 2],
+          secondaryColor:  ['#BBC1CD', '#9A7A55'][currentIndex % 2],
+          detailColor:     ['#FFFFFF', '#750310'][currentIndex % 2],
+          title:           data.title,
+          title:           data.summary,
+          emojis:          new Handlebars.SafeString(reactions),
+          user:            r.user,
+          perRow:          halfsQtyForScreen,
+          url:             data.url,
+        };
       } else if(r.data.type === 'twitter') {
         console.log(r);
         return {
@@ -48,6 +67,23 @@
           perRow: halfsQtyForScreen,
         }
       } else if(r.data.type === 'vimeo' || r.data.type === 'youtube') {
+        const reactions = r.reactions.map(processReaction).join('');
+        const data = r.data.data;
+        return {
+          extraClass: r.data.type === 'youtube' ? 'youtube' : '',
+          imageUrl: data.thumbnail_data.url,
+          backgroundColor: data.thumbnail_data.background_color,
+          primaryColor: data.thumbnail_data.primary_color,
+          secondaryColor: data.thumbnail_data.secondary_color,
+          detailColor: data.thumbnail_data.detail_color,
+          title: data.title,
+          summary: data.description,
+          emojis: new Handlebars.SafeString(reactions),
+          user: r.user,
+          perRow: halfsQtyForScreen,
+          url: data.url,
+        }
+      } else if(r.data.type === 'spotify') {
         const reactions = r.reactions.map(processReaction).join('');
         const data = r.data.data;
         return {
@@ -101,3 +137,16 @@ function processReaction(r) {
   }
   return r.repr;
 }
+
+$(window).on('load resize', function() {
+  $('iframe[src*="embed.spotify.com"]').each(function() {
+    var $this = $(this);
+    $this.css('width', $this.parent().css('width'));
+    $this.attr('src', $this.attr('src'));
+    $this.removeClass('loaded');
+
+    $this.on('load', function(){
+      $this.addClass('loaded');
+    });
+  });
+});
